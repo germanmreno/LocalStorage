aabbf2704dbe140b66eec3f6b296d35bbff5ead8
Germán Moreno <grmmrno@gmail.com>
2020-09-22 10:14:12 -0400

    Proyecto final

--- js/app.js
+++ js/app.js
@@ -6,6 +6,12 @@ let tweets = [];
 eventListeners();
 function eventListeners() {
     formulario.addEventListener("submit", agregarTweet);
+
+    document.addEventListener("DOMContentLoaded", () => {
+        tweets = JSON.parse(localStorage.getItem("tweets")) || [];
+
+        renderTweets();
+    });
 }
 
 function agregarTweet(e) {
@@ -18,19 +24,44 @@ function agregarTweet(e) {
         return;
     }
 
-    tweets = [...tweets, tweet];
-    renderTweets(tweets);
+    const tweetObj = {
+        id: Date.now(),
+        tweet,
+    };
+
+    tweets = [...tweets, tweetObj];
+    renderTweets();
+    formulario.reset();
 }
 
-function renderTweets(tw) {
+function renderTweets() {
     limpiarHTML();
 
-    tw.forEach((tweet) => {
-        const newTweet = document.createElement("p");
-        newTweet.textContent = tweet;
+    if (tweets.length > 0) {
+        tweets.forEach((tweet) => {
+            const btnEliminar = document.createElement("a");
+            btnEliminar.classList.add("borrar-tweet");
+            btnEliminar.textContent = "X";
+            btnEliminar.onclick = () => {
+                borrarTweet(tweet.id);
+            };
 
-        listaTweets.appendChild(newTweet);
-    });
+            const newTweet = document.createElement("li");
+            newTweet.textContent = tweet.tweet;
+            newTweet.appendChild(btnEliminar);
+            listaTweets.appendChild(newTweet);
+        });
+    }
+    sincronizarStorage();
+}
+
+function sincronizarStorage() {
+    localStorage.setItem("tweets", JSON.stringify(tweets));
+}
+
+function borrarTweet(id) {
+    tweets = tweets.filter((tweet) => tweet.id !== id);
+    renderTweets();
 }
 
 function limpiarHTML() {
